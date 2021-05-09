6a6e87d7a25882553dddef56c991c068cdc1e103
ahmetceylan90 <ceylan90ahmet@gmail.com>
2021-05-09 14:17:59 +0300

    değişkenler dersi

--- Introduction/Lesson1/variables/ornek.py
+++ Introduction/Lesson1/variables/ornek.py
@@ -1,4 +1,43 @@
+
 # yorum satırı eklendi. 
 # alt + z alt satıra satıra aşagı kaydırr
 
 print("Hello world")
+
+########### değişken tanımlama kuralları
+# 1- sayi ile baslayamaz
+# 2- birden fazla kelime ise küçük harfle başlayıp 
+# her kelimenin ilk harfi büyük olması tavsiye edilir
+# 3- tanımlı klemeiler değişken ismi olarak kullanilmaz
+# 4- türkçe karakter kullanmayiniz
+
+############### global alan içerisinde değişken tanımlama
+
+userName = "ahmet"
+
+{
+    # bu alan globaldir
+    b = 1
+    # b değişkenine sadece burdaken ulaşılabilir
+    {
+        c = 2
+        print (b + c)  = 3 ## bu sonucu verir
+    }
+    print(b+c) # hata alırız çünkü c bir alt kümede değişken olarak oluşturldugu için
+}
+
+############ sayısal veri tipleri
+# int,float,
+age = 10.21
+wight = 79
+print (age,wight, sep="***")
+
+print (type(age))
+
+# shift + alt + f --> kodları formatlar
+
+########### mantıksal veri tipleri
+retval = 2 > 10
+print (retval)
+
+########## \n metin içinde bir alt satıra geçmeyi sağlar
