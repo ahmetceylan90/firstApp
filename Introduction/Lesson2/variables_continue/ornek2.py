b55223fbf3f8da433c3a0e72474fad630813d83f
ahmetceylan90 <ceylan90ahmet@gmail.com>
2021-05-16 10:54:12 +0300

    lesson2 eklendi

---
+++ Introduction/Lesson2/variables_continue/ornek2.py
@@ -0,0 +1,24 @@
+# Aritmetik Operatörler
+
+sayi1 = 120
+sayi2 = 50
+toplam = sayi1 + sayi2
+print("toplama işlemi sonucu : \n", toplam)
+
+# kullanicidan parametre almak
+# input
+# input ile alınan her deger string olarak default gelir
+
+num1 = input("ilk sayi")
+print(num1)
+
+fark = sayi1 - sayi2
+print(fark)
+
+carp = sayi1 * sayi2
+bol = sayi1 / sayi2 
+mod = sayi1 % sayi2
+
+print(carp)
+print(bol)
+print(mod)
